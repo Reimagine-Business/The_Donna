52296e389a645b001c90de9386c8c12e889f36de
reimaginebusiness <reimaginebusiness2025@gmail.com>
2025-11-20 13:42:19 +0530

    Update next.config.ts

--- donna-clean/next.config.ts
+++ donna-clean/next.config.ts
@@ -1,7 +1,6 @@
 // next.config.js
 const nextConfig = {
-  // This is the ONLY way to disable Turbopack in Next.js 16 on Vercel right now
-  turbopack: {},
+  turbopack: {},   // this disables Turbopack and forces stable Webpack
 }
 
 module.exports = nextConfig
